74bb3ce10abdbeef3a0d8e2b8c1eedd7d4f52bc1
sweta yadav <swetakumari0363@gmail.com>
2025-05-14 23:02:51 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import NavBar from './components/NavBar.jsx'
+import ExperienceSection from './sections/ExperienceSection.jsx'
+import FeatureCards from './sections/FeatureCards.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
 const App = () => {
   return (
     <>
+    <NavBar></NavBar>
      <Hero></Hero>
      <ShowcaseSection></ShowcaseSection>
+     <FeatureCards
+     ></FeatureCards>
+     <ExperienceSection></ExperienceSection>
     </>
   )
 }
